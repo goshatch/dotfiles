864b405ea9ad9d0fb80018745e07c3806c174955
Gosha Tcherednitchenko <git@gosha.net>
2025-08-18 16:39:59 +0100

    feat(emacs): add lispy+lispyville

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -63,7 +63,7 @@
         +onsave
         +lsp)
        ;;god               ; run Emacs commands without modifier keys
-       ;;lispy             ; vim for lisp, for people who don't like vim
+       lispy               ; vim for lisp, for people who don't like vim
        multiple-cursors    ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
